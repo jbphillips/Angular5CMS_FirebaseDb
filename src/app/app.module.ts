017543cbb88faa30d8462b2bf4a86f1654bb2276
ROCHESTER\JPhillips <jphillips@gleason.com>
2018-06-19 15:25:56 -0400

    connect to firebase imported json starter added login...broke, must fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AngularFireModule } from 'angularfire2';
 import { PagesListComponent } from './pages-list/pages-list.component';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagesListComponent
+    PagesListComponent,
+    LoginPageComponent//,
+    //AppRoutingModule
   ],
   imports: [
     BrowserModule,
