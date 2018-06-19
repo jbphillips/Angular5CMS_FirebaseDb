727455bd6d30df5c214c6b114a3b89e4ed82e990
ROCHESTER\JPhillips <jphillips@gleason.com>
2018-06-19 14:18:27 -0400

    initial firebase practice commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDkwlV0ngdhmoXRKU7kf0fhXSX3CX8RPeI",
+    authDomain: "cms-database-e9677.firebaseapp.com",
+    databaseURL: "https://cms-database-e9677.firebaseio.com",
+    projectId: "cms-database-e9677",
+    storageBucket: "cms-database-e9677.appspot.com",
+    messagingSenderId: "144366592625"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDkwlV0ngdhmoXRKU7kf0fhXSX3CX8RPeI",
+    authDomain: "cms-database-e9677.firebaseapp.com",
+    databaseURL: "https://cms-database-e9677.firebaseio.com",
+    projectId: "cms-database-e9677",
+    storageBucket: "cms-database-e9677.appspot.com",
+    messagingSenderId: "144366592625"
+  }
 };
